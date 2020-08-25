dd2b7fc80f237b8b9ade605411316a30e51692fe
Peter Anton <peteranton1@yahoo.com>
2020-08-26 00:07:54 +0100

    step02a parser

---
+++ monkey/ast/ast.go
@@ -0,0 +1,66 @@
+// Package ast ast/ast.go
+package ast
+
+import (
+	"monkey/token"
+)
+
+// Node base type
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement subtype
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression subtype
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program struct
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral interface method
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement struct
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+// statementNode interface method
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral interface method
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier struct
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+// statementNode interface method
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral interface method
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
---
+++ monkey/parser/parser.go
@@ -0,0 +1,111 @@
+// Package parser parser/parser.go
+package parser
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+// Parser struct
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+	errors    []string
+}
+
+// New returns a parser
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	// Read two tokens, so curToken
+	// and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+// Errors getter function
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be '%s', "+
+		"got='%s', col=%d", t, p.peekToken.Type, p.peekToken.Col)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// ParseProgram root node function
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: skip expression for now
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
