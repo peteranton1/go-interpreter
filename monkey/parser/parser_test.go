acd20ce3155015b6c3401faa74f6e18878f402fb
Peter Anton <peteranton1@yahoo.com>
2020-08-27 21:16:15 +0100

    step02f parser

--- monkey/parser/parser_test.go
+++ monkey/parser/parser_test.go
@@ -352,21 +352,88 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	return true
 }
 
+func testLiteralExpression(
+	t *testing.T,
+	exp ast.Expression,
+	expected interface{},
+) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifier(t, exp, v)
+		// case bool:
+		// 	return testBooleanLiteral(t, exp, v)
+	}
+	t.Errorf("type of exp not handled. got=%T", exp)
+	return false
+}
+
+func testInfixExpression(t *testing.T,
+	exp ast.Expression,
+	left interface{},
+	operator string,
+	right interface{},
+) bool {
+
+	opExp, ok := exp.(*ast.InfixExpression)
+	if !ok {
+		t.Errorf("exp is not ast.InfixExpression, got=%T(%s)",
+			exp, exp)
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Left, left) {
+		return false
+	}
+	if opExp.Operator != operator {
+		t.Errorf("exp.Operator is not '%s', got=%q",
+			operator, opExp.Operator)
+		return false
+	}
+	return true
+}
+
 func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 	integ, ok := il.(*ast.IntegerLiteral)
 	if !ok {
-		t.Errorf("il not *ast.IntegerLiteral, got=%T", il)
+		t.Errorf("il not *ast.IntegerLiteral. got=%T", il)
 		return false
 	}
+
 	if integ.Value != value {
-		t.Errorf("integ.Value not '%d', got=%d", value, integ.Value)
+		t.Errorf("integ.Value not %d. got=%d", value, integ.Value)
 		return false
 	}
-	valueString := fmt.Sprintf("%d", value)
-	if integ.TokenLiteral() != valueString {
-		t.Errorf("integ.TokenLiteral() not '%s', got=%s",
-			valueString, integ.TokenLiteral())
+
+	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
+		t.Errorf("integ.TokenLiteral not %d. got=%s", value,
+			integ.TokenLiteral())
 		return false
 	}
+
+	return true
+}
+
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Identifier. got=%T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value,
+			ident.TokenLiteral())
+		return false
+	}
+
 	return true
 }
