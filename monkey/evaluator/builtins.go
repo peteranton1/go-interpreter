14a7ed1aa8a38c92f72f0325a7adeacb3cd8603c
Peter Anton <peteranton1@yahoo.com>
2020-09-01 00:27:16 +0100

    step04f eval

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -1,7 +1,10 @@
 // Package evaluator evaluator/builtins.go
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -92,6 +95,14 @@ var builtins = map[string]*object.Builtin{
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
