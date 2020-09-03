5dea924e32d72c71bff530bd253fa900d285deff
Peter Anton <peteranton1@yahoo.com>
2020-09-03 21:46:39 +0100

    compiler01a

---
+++ monkey/vm/vm.go
@@ -0,0 +1,80 @@
+// Package vm vm/vm.go
+package vm
+
+import (
+	"fmt"
+	"monkey/code"
+	"monkey/compiler"
+	"monkey/object"
+)
+
+// StackSize const
+const StackSize = 2048
+
+// VM struct
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+	stack        []object.Object
+	// always points to next value.
+	// Top of stack is stack[sp-1].
+	sp int
+}
+
+// New func
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		instructions: bytecode.Instructions,
+		constants:    bytecode.Constants,
+		stack:        make([]object.Object, StackSize),
+		sp:           0,
+	}
+}
+
+// StackTop func
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+// Run func
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+
+		case code.OpAdd:
+			right := vm.pop()
+			left := vm.pop()
+			leftValue := left.(*object.Integer).Value
+			rightValue := right.(*object.Integer).Value
+			result := leftValue + rightValue
+			vm.push(&object.Integer{Value: result})
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+	vm.stack[vm.sp] = o
+	vm.sp++
+	return nil
+}
+
+func (vm *VM) pop() object.Object {
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+	return o
+}
